2f4364c5c794e1175e6f27e0c615ac7ebccddcc5
Julia Wang <juliawang105@gmail.com>
2020-12-09 08:56:06 -0800

    Begin Day 2 challenge

---
+++ day2.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const data = fs.readFileSync("./day2_julia.txt", { encoding: "utf8" });
+
+data = data.split("\n"); 
+
+// el = [ "13-14 f: ffff " ]
+let start;
+let end; 
+let letter; 
+for(let i = 0; i < el.length; i ++ ){
+  let currString = el[i]; 
+  for(let j = 0; j < currString.length; j ++ ){
+    
+  }
+}

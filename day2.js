3e631b87abd041589e216c9b59d32cbbf4a3d059
Michelle Kim <x1michelleskim@gmail.com>
2020-12-11 08:35:52 -0800

    Solve part 2 of day 2

--- day2.js
+++ day2.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 
-let data = fs.readFileSync("./day2_julia.txt", { encoding: "utf8" });
+let data = fs.readFileSync("./day2_mish.txt", { encoding: "utf8" });
 
 data = data.split("\n"); 
+
+// PART 1
 
 function validPW(data){
   let total = 0;
@@ -23,5 +25,27 @@ function validPW(data){
   return total; 
 }
 
-console.log(validPW(data));
+// console.log(validPW(data));
+
+// PART 2
+
+function validPW2(data) {
+  let total = 0;
+
+  // el = [ "13-14 f: ffff " ]
+  for (let i = 0; i < data.length; i++) {
+    let currString = data[i];
+    let lines = currString.split(" "); //[ ["13-14"], ["f:"], ["ffff"]]
+    let range = lines[0].split("-");
+    let letter = lines[1][0];
+    let count = 0;
+    
+    if(lines[2][range[0]-1] === letter) count ++;
+    if(lines[2][range[1]-1] === letter) count ++;
+    if (count === 1) total ++;
+  }
+  return total;
+}
+
+console.log(validPW2(data));
 

43d9f9a49470b168402684ae65a4de0dc98ece9c
Michelle Kim <x1michelleskim@gmail.com>
2021-01-26 16:11:33 -0800

    Work on puzzle

--- day4/day4.js
+++ day4/day4.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { nextTick } = require('process');
 
 const data = fs.readFileSync('./day4_mish.txt', { encoding: 'utf8' });
 
@@ -27,4 +28,36 @@ function cheatValidPassport(data) {
     return valid;
 }
 
-console.log(cheatValidPassport(data));
+// console.log(cheatValidPassport(data));
+
+// Part 2
+
+function cheatValidAdvanced(data) {
+    let lines = data.split("\n");
+    // console.log(lines[2] === '');
+    let batches = [];
+    let valid = 0;
+    let necessary = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
+    for (let i = 0; i < lines.length; i++) {
+        if (lines[i] === '') {
+            // console.log('prev', batches)
+            batches = batches.join(" ");
+            let present = necessary.every(check => {
+                return batches.includes(check);
+            })
+            if (present) {
+                batches = batches.split(" ");
+                let sets = batches.map((ele) => ele.split(":"));
+                
+            }
+            
+            batches = [];
+        } else {
+            batches.push(lines[i]);
+        }
+    }
+    // console.log(batches);
+    return valid;
+}
+
+console.log(cheatValidAdvanced(data));
